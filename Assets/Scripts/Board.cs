9af1b1e6a6189104d031c197a3b7a9fcf50a0184
Oksana Pavliuk <oksanapavliuk05@gmail.com>
2026-02-24 10:51:42 +0200

    Add assets and board generation

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private GameObject[,] allTiles;
+    //private BackgroundTile[,] allTiles; // масив для тайлів розміром нашого поля
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        allTiles = new GameObject[width, height];  // масив для тайлів розміром нашого поля
+        //allTiles = new BackgroundTile[width, height];  // масив для тайлів розміром нашого поля
+        SetUp();
+    }
+    private void SetUp()
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for(int j =0; j < height; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j);
+                GameObject newTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity); 
+                //Instantiate(tilePrefab, tempPosition, Quaternion.identity); 
+                allTiles[i, j] = newTile;
+            }
+        }
+    }
+}
